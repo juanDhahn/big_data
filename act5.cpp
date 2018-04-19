2055d7d2fec27732bb18f296929bebd43cf0bb00
Guillermo Iglesias <guillermo.iglesias.90@gmail.com>
2018-04-19 18:21:45 -0300

    act5 c++ done

--- act5.cpp
+++ act5.cpp
@@ -1,4 +1,7 @@
+/* Compile: g++ act5.cpp -o act5.exe -lpq && ./act5.exe */
 #include <iostream>
+#include <string.h>
+#include <map>
 #include <postgresql/libpq-fe.h>
 using namespace std;
 
@@ -12,19 +15,68 @@ int main()
     cout << "Unable to connect to database\n";
     cout << PQerrorMessage(dbconn);
   }
-  else{
+  else {
     cout << "Connection to database OK\n";
   }
 
+  // Execute query
+  PGresult *query; 
+  query = PQexec(dbconn, "BEGIN");
+  PQclear(query);
+  query = PQexec(dbconn, "DECLARE cur_edad CURSOR FOR SELECT edad FROM personas1");
 
-  // Result holder
-  // PGresult *query;
+  // Declare map for results
+  map<string,int> map_edades;
 
-  // Execute a query
-  // query = PQexec(dbconn, "select * from mytable");
+  for (int i = 0; i < 10000; i++) {
+    PQclear(query);
+    query = PQexec(dbconn, "FETCH FORWARD 3500 in cur_edad");
+    
+    for (int j = 0; j < int(PQntuples(query)); j++) {
 
-  // Use the values
-  // printf ("%s\n", PQgetvalue(query, 0, 1));
+      string key = PQgetvalue(query, j, 0);
+      
+      // Check if key exist in map
+      if(map_edades.find(key) != map_edades.end()) {
+        map_edades[key] = map_edades[key] + 1;
+      }
+      // if not create new key
+      else {
+        map_edades.insert(make_pair(key,1));
+      }
+
+    }
+
+
+    /*
+    // Check if query is OK
+    if (strlen(PQresultErrorMessage(query)) == 0) {
+      
+      // Print result
+      for (int j = 0; j < int(PQntuples(query)); j++) {
+        cout << PQgetvalue(query, j, 0) << endl;
+      }
+
+      cout << "Fetched rows: " << PQntuples(query) << endl;
+
+    }
+    else {  
+      cout << PQresultErrorMessage(query);
+    }
+    */
+  }
+  
+  for (map<string,int>::iterator it = map_edades.begin(); it != map_edades.end(); ++it){
+    cout << it -> first << " -> " << it -> second << endl;
+  }
+    
+  
+  // Close cursor and clear query
+  PQclear(query);
+  query = PQexec(dbconn, "CLOSE cur_edad");
+  PQclear(query);
+  query = PQexec(dbconn, "COMMIT");
+  PQclear(query);
 
   // Close connection
   PQfinish(dbconn);
